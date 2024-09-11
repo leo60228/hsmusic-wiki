55dad23c4b65f9040b6b3e97c05813263b009ef7
leo60228 <leo@60228.dev>
2024-09-11 18:19:50 -0400

    address review

--- src/static/js/client/sidebar-search.js
+++ src/static/js/client/sidebar-search.js
@@ -330,11 +330,11 @@ export function addPageListeners() {
   info.searchInput.addEventListener('drop', handleDroppedIntoSearchInput);
 
   info.searchInput.addEventListener('keydown', domEvent => {
-    if (domEvent.key === 'ArrowDown' && info.searchInput.value.length === info.searchInput.selectionStart) {
+    if (domEvent.key === 'ArrowDown') {
       const elem = info.results?.firstChild;
       if (elem && !elem.classList.contains('wiki-search-no-results')) {
         domEvent.preventDefault();
-        elem.focus();
+        elem.focus({focusVisible: true});
       }
     }
   });
@@ -790,13 +790,13 @@ function generateSidebarSearchResultTemplate(slots, info) {
       const elem = link.nextElementSibling;
       if (elem) {
         domEvent.preventDefault();
-        elem.focus();
+        elem.focus({focusVisible: true});
       }
     } else if (domEvent.key === 'ArrowUp') {
       domEvent.preventDefault();
       const elem = link.previousElementSibling;
       if (elem) {
-        elem.focus();
+        elem.focus({focusVisible: true});
       } else {
         info.searchInput.focus();
       }
